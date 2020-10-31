ae5f19f8c2ee2444d38f06d013e526064903f70f
yakataN <zeronosozaiatsume@gmail.com>
2020-10-31 09:59:12 +0900

    Add ch02_2

--- ch02_2/01_problem_p42.cpp
+++ ch02_2/01_problem_p42.cpp
@@ -4,50 +4,26 @@ typedef long long ll;
 typedef pair<ll, ll> lpair;
 const ll MOD = 1e9 + 7;
 const ll INF = 1e18;
-#define REP(i,m,n) for(ll i = (m); i < (ll)(n); i++)
-#define rep(i,n) REP(i, 0, n)
-#define rrep(i,n) REP(i,1,n+1)
+#define REP(i, m, n) for (ll i = (m); i < (ll)(n); i++)
+#define rep(i, n) REP(i, 0, n)
+#define rrep(i, n) REP(i, 1, n + 1)
 #define print(x) cout << (x) << endl;
 
-const int max_n = 110;
-const int max_w = 10010;
-int N, W;
-// dp[i][j]:= i番目まで見て、残りのweightがjある時の価値の最大
-int dp[max_n][max_w];
-int w[max_n], v[max_n];
-
-int rec(int i, int j)
-{
-  if (dp[i][j] >= 0)
-  {
-    return dp[i][j];
-  }
-  int res;
-
-  if (i==N)
-  {
-    // 使い切ったら0を返してStopIteration
-    res = 0;
-  }
-  else if (j < w[i])
-  {
-    // 使わない
-    res = rec(i + 1, j);
-  }
-  else
-  {
-    // 使うかもしれない
-    res = max(rec(i + 1, j), rec(i + 1, j - w[i]) + v[i]);
-  }
-
-  return dp[i][j] = res;
-}
+const int values[6] = {1, 5, 10, 50, 100, 500};
+ll coins[6];
+ll a;
 
 int main()
 {
-  cin >> N >> W;
-  rep(i,N) cin >> w[i] >> v[i];
+    rep(i, 6) cin >> coins[i];
+    cin >> a;
 
-  memset(dp, -1, sizeof(dp));
-  cout << rec(0, W) << endl;
+    ll ans = 0;
+    for (int i = 5; i >= 0; i--)
+    {
+        ll want = a/values[i];
+        ans += min(want, coins[i]);
+        a -= values[i] * min(want, coins[i]);
+    }
+    cout << ans << endl;
 }
--- ch02_2/02_problem_p43.cpp
+++ ch02_2/02_problem_p43.cpp
@@ -8,4 +8,32 @@ const ll INF = 1e18;
 #define rep(i,n) REP(i, 0, n)
 #define rrep(i,n) REP(i,1,n+1)
 #define print(x) cout << (x) << endl;
+typedef pair<int, int> P;
 
+int N;
+const int MAX_N = 101010;
+P tasks[MAX_N];
+
+int main()
+{
+  cin >> N;
+  rep(i,N)
+  {
+    ll s,t;
+    cin >> s >> t;
+    tasks[i] = make_pair(t,s);
+  }
+
+  sort(tasks, tasks+N);
+  ll nt = -1;
+  int ans = 0;
+  rep(i,N)
+  {
+    if (nt < tasks[i].second)
+    {
+      nt = tasks[i].first;
+      ans++;
+    }
+  }
+  cout << ans << endl;
+}
